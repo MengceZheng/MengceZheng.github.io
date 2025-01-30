74eb00e89495f16fb2d9e2dcf5e2e4fa435587bc
Mengce Zheng <mengce.zheng@gmail.com>
2025-01-30 20:34:22 +0100

    Optimize Google Scholar crawler workflow and script

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,23 +1,32 @@
-from scholarly import scholarly
-import jsonpickle
+import logging
+from scholarly import scholarly, ProxyGenerator
 import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-name = author['name']
-author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=2))
-os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
-    json.dump(author, outfile, ensure_ascii=False)
+logging.basicConfig(level=logging.INFO)
 
-shieldio_data = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['citedby']}",
-}
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
-    json.dump(shieldio_data, outfile, ensure_ascii=False)
+try:
+    pg = ProxyGenerator()
+    pg.FreeProxies()
+    scholarly.use_proxy(pg)
+
+    author = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+    author['updated'] = str(datetime.now())
+    author['publications'] = {v['author_pub_id']: v for v in author['publications']}
+
+    os.makedirs('results', exist_ok=True)
+    with open('results/gs_data.json', 'w') as outfile:
+        json.dump(author, outfile, ensure_ascii=False, indent=2)
+
+    shieldio_data = {
+        "schemaVersion": 1,
+        "label": "citations",
+        "message": f"{author.get('citedby', 0)}",
+    }
+    with open('results/gs_data_shieldsio.json', 'w') as outfile:
+        json.dump(shieldio_data, outfile, ensure_ascii=False, indent=2)
+
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
